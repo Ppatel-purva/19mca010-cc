58d921285d7046e27b82b41292c8a385180ffdb3
Nikunj4368 <69626678+Nikunj4368@users.noreply.github.com>
2020-12-02 16:51:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Register</title>
+	<title>Register Page</title>
 </head>
 <body>
 	<div>
 		<form method="post" action="reg.php">
-			<h1>Student data using db instance</h1>
+			<h1>Student Data</h1>
 			<input type="text" name="name" placeholder="Enter your name"> <br><br>
 			<input type="text" name="email" placeholder="Enter your email"> <br><br>
 			<input type="password" name="passwd" placeholder="Enter your password"> <br><br>
